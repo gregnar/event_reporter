89fde738ddf6dc39af2bc8d3bf64abf50550cd00
Greg Narajka <gregnar@gmail.com>
2014-09-29 15:06:42 -0600

    fixed attendees and repository

--- lib/attendee_repository.rb
+++ lib/attendee_repository.rb
@@ -1,7 +1,11 @@
+require_relative 'attendee'
+
 class AttendeeRepository
 
-  def populate_repository(csv)
-      
+  def populate_repository(csv_array)
+    @repository = csv_array.map do |hash|
+      Attendee.new(hash)
+    end
   end
 
 end
--- lib/csv_parser.rb
+++ lib/csv_parser.rb
@@ -1,4 +1,5 @@
 require 'csv'
+require_relative 'attendee_repository' #temporary dependency to make sure it works
 class CSVParser
 
   def make_csv
@@ -8,3 +9,7 @@ class CSVParser
     csv = CSV.table(file, :header_converters => :symbol, :converters => :all)
   end
 end
+
+csv = CSVParser.new.make_csv
+repo = AttendeeRepository.new.populate_repository(csv)
+puts repo[1].first_name
