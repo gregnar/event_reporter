56cf8637c47de8ed1fc4be673b56ab8c677bf2c9
Josh Cheek <josh.cheek@gmail.com>
2014-09-30 10:35:09 -0600

    Begin implementing the CLI

--- lib/cli.rb
+++ lib/cli.rb
@@ -1,6 +1,21 @@
 require 'csv_parser'
+require 'attendee_repository'
+
 class CLI
   def initialize
-    @attendees_list = CSVPaser.new.make_csv
+  end
+
+  def evaluate(user_input)
+    tokens  = user_input.split
+    command = tokens.first
+    if command == "load"
+      raw_attendees = CSVParser.new.load_csv tokens.last
+      repo = AttendeeRepository.new
+      repo.populate_repository raw_attendees
+    else
+      0
+    end
+    require "pry"
+    binding.pry
   end
 end
--- lib/csv_parser.rb
+++ lib/csv_parser.rb
@@ -2,6 +2,7 @@ require 'csv'
 require_relative 'attendee_repository' #temporary dependency to make sure it works
 class CSVParser
   def load_csv(file_name)
+    file_name = File.join "csv", file_name
     csv = CSV.table(file_name, :header_converters => :symbol, :converters => :all)
   end
 end
