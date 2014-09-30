093a66a52cd715c032941ab7fcfc97fef8ee7bfc
Josh Cheek <josh.cheek@gmail.com>
2014-09-30 10:20:55 -0600

    Add a CLI test

--- test/cli_test.rb
+++ test/cli_test.rb
@@ -1,5 +1,46 @@
 require_relative 'test_helper'
-require_relative '../lib/cli'
+require 'cli'
 
 class CLITest < Minitest::Test
+  def test_happy_path
+    cli = CLI.new
+
+    # load event_attendees.csv
+    cli.evaluate("load event_attendees.csv")
+
+    # queue count should return 0
+    assert_equal 0, cli.evaluate("queue count")
+
+    # find first_name John
+    johns = cli.evaluate("find first_name John")
+    assert_equal 63, johns.size
+
+    johns.each do |output_line|
+      assert_includes "john", output_line
+    end
+
+    # queue count should return 63
+    assert_equal 63, cli.evaluate("queue count")
+
+    # queue clear
+    cli.evaluate "queue clear"
+
+    # queue count should return 0
+    assert_equal 0, cli.evaluate("queue count")
+
+    # help should list the commands
+    command_list = cli.evaluate "help"
+    assert_includes "help",        command_list
+    assert_includes "queue count", command_list
+    assert_includes "find",        command_list
+    assert_includes "load",        command_list
+
+    # help queue count should explain the queue count function
+    command_list = cli.evaluate("help queue count")
+    assert_includes "queue count:"
+
+    # help queue print should explain the printing function
+    command_list = cli.evaluate("help queue print")
+    assert_includes "queue print:"
+  end
 end
--- test/test_helper.rb
+++ test/test_helper.rb
@@ -1,3 +1,5 @@
 gem 'minitest'
 require 'minitest/autorun'
 require 'minitest/pride'
+
+$LOAD_PATH.unshift File.expand_path('../../lib', __FILE__)
