e4ffce3e30e92e05d17c5697ec06e195be1acc84
Josh Cheek <josh.cheek@gmail.com>
2014-09-30 10:00:07 -0600

    Can run the program
    Also fixes load path so that we can use
    require instead of require_relative
    or require "./..."

--- lib/cli.rb
+++ lib/cli.rb
@@ -1,23 +1,6 @@
-require_relative 'csv_parser'
+require 'csv_parser'
 class CLI
-
   def initialize
     @attendees_list = CSVPaser.new.make_csv
   end
-
-
-
-
-
-end
-
-
-gets_command
-interpreted_command = CommandInterpreter.interpret(@command)
-UserCommands.new.execute_command(interpreted_command)
-
-
-
-def gets_command
-  @command = gets.chomp
 end
--- lib/csv_parser.rb
+++ lib/csv_parser.rb
@@ -1,12 +1,11 @@
 require 'csv'
 require_relative 'attendee_repository' #temporary dependency to make sure it works
 class CSVParser
-
   def load_csv(file_name)
     csv = CSV.table(file_name, :header_converters => :symbol, :converters => :all)
   end
 end
 
-csv = CSVParser.new.make_csv
-repo = AttendeeRepository.new.populate_repository(csv)
-puts repo[1].first_name
+# csv = CSVParser.new.make_csv
+# repo = AttendeeRepository.new.populate_repository(csv)
+# puts repo[1].first_name
