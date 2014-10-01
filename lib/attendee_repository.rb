4edcdd40396799b543ca26fe7123420d347097ec
Herbert Joseph <techniquea2z@gmail.com>
2014-10-01 16:12:26 -0600

    attendee repo fix merge

--- lib/attendee_repository.rb
+++ lib/attendee_repository.rb
@@ -17,10 +17,6 @@ class AttendeeRepository
     result = repo.find_all do |attendee|
       attendee.send(attribute.to_sym).to_s.downcase == criteria.downcase
     end
-<<<<<<< HEAD
-    prepare_for_printing(result)
-=======
->>>>>>> ff5c301eed6b4df9317f23e98f2dc0833f7f3dbb
     result
   end
 
